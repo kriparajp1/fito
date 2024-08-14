46d7ab817b7508586148f5dadf29c5a1518d7f37
kriparajp1 <kripp.me@outlook.com>
2024-08-14 09:30:00 +0530

    google call back

--- controller/user/googleAuth.js
+++ controller/user/googleAuth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID:process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://www.fitomart.shop/auth/google/callback",
     },
     async (request, accessToken, refreshToken, profile, done) => {
       try {
